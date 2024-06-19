831d1469062f1b325cd20a3ed7ab0a1d753f1433
Shohruh Sharipov <~!QAZxsw2>
2024-06-19 22:35:17 +0500

    added logging

--- src/main/java/org/onevizion/booksample/service/BookServiceImpl.java
+++ src/main/java/org/onevizion/booksample/service/BookServiceImpl.java
@@ -1,5 +1,6 @@
 package org.onevizion.booksample.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.onevizion.booksample.domain.Book;
 import org.onevizion.booksample.dto.AuthorBookCountDTO;
 import org.onevizion.booksample.dto.BookDTO;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class BookServiceImpl implements BookService {
     @Autowired
@@ -20,28 +22,35 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public List<BookDTO> getAllBooksSortedByTitleDesc() {
-        return bookRepository.findAllByOrderByTitleDesc().stream()
+        log.debug("Fetching all books sorted by title in descending order.");
+        var books = bookRepository.findAllByOrderByTitleDesc().stream()
                 .map(MappingUtil::convertToDTO)
                 .collect(Collectors.toList());
+        log.info("Fetched {} books.", books.size());
+        return books;
     }
 
     @Override
     public void addBook(BookDTO bookDTO) {
         Book book = MappingUtil.convertToEntity(bookDTO);
         bookRepository.save(book);
+        log.info("Added new book: {}", book);
     }
 
     @Override
     public Map<String, List<BookDTO>> getBooksGroupedByAuthor() {
-        return bookRepository.findAll().stream()
+        log.debug("Grouping books by author.");
+        Map<String, List<BookDTO>> groupedBooks = bookRepository.findAll().stream()
                 .collect(Collectors.groupingBy(Book::getAuthor, Collectors.mapping(MappingUtil::convertToDTO, Collectors.toList())));
+        log.info("Grouped books by author: {}", groupedBooks.keySet());
+        return groupedBooks;
     }
 
     @Override
     public List<AuthorBookCountDTO> getTopAuthorsByCharacterCount(char character) {
-        character = Character.toLowerCase(character);
-        char finalCharacter = character;
-        return bookRepository.findAll().stream()
+        log.debug("Getting top authors by character count in book titles.");
+        char finalCharacter = Character.toLowerCase(character);
+        List<AuthorBookCountDTO> topAuthors = bookRepository.findAll().stream()
                 .collect(Collectors.groupingBy(Book::getAuthor))
                 .entrySet().stream()
                 .map(entry -> {
@@ -55,5 +64,7 @@ public class BookServiceImpl implements BookService {
                 .sorted(Comparator.comparingLong(AuthorBookCountDTO::getCount).reversed())
                 .limit(10)
                 .collect(Collectors.toList());
+        log.info("Top authors by character count '{}': {}", character, topAuthors);
+        return topAuthors;
     }
 }
